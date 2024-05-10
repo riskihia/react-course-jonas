504e7d83f4b4c6624036b108f6e341bd244b6fae
Riski Rahmat Hia <riskirahmathia3@gmail.com>
2024-05-10 07:40:39 +0700

    add data, createt handle previous and next

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  let step = 1;
+  console.log(step);
+
+  function handlePrevious() {
+    step--;
+  }
+  function handleNext() {
+    step++;
+    console.log(step);
+  }
+
   return (
-    <div>
-      <h1>My React App!</h1>
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <div className="message">
+        Step {step} : {messages[step - 1]}
+      </div>
+
+      <div className="buttons">
+        <button
+          onClick={handlePrevious}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
